9607041442877bdc41035052f4788dc610e83b07
Dark System <darksystemcoder@gmail.com>
2023-06-26 19:39:33 -0400

    Final push, icons added

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo 'Starting Installation'
+npm install
+echo 'Installing The Packging Module'
+npm install -g pkg
+echo 'Creating Directories. Ignore Errors from mkdir.'
+echo 'mkdir /system /system/webapps&& echo `{}`> /system/webapps/apps.json'| sudo bash
+echo 'Builing Package'
+mkdir ./builds
+pkg ./shell.js
+echo 'Moving Distrubutibles'
+mv ./shell-linux ./builds/
+mv ./shell-win.exe ./builds/
+mv ./shell-macos ./builds/   
+npm remove -g pkg
+echo 'Installation Complete. The executables are in the builds folder'
+if [ $(uname) == 'Linux' ]; then
+    echo 'mv ./builds/shell-linux ./builds/webapps && mv ./builds/webapps /bin/'| sudo bash
+fi
---
+++ shell.js
@@ -0,0 +1,222 @@
+const readline = require('readline-sync')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+const os = require('os')
+const { exec } = require('child_process');
+//const { app, BrowserWindow } = require('electron')
+//const nativeImage = require('electron').nativeImage
+const download = require('download')
+var helperFunctions = {
+    parseArguments: (flags) => {
+        var args = {}
+        process.argv.forEach(function (val) {
+            let pos = process.argv.indexOf(val)
+            if (!(flags.indexOf(val.replaceAll('-', '')) == -1)) {
+                args[flags[flags.indexOf(val.replaceAll('-', ''))]] = process.argv[pos + 1]
+            }
+
+        })
+        return args
+    },
+    processArgs: () => {
+        var args = helperFunctions.parseArguments(['install', 'n', 'name', 'run', 'u', 'url', 'help', 'h', 'remove', 'r', 'i','appDir'])
+        //console.log(args)
+        if (args.name == undefined) {
+            args.name = args.n
+            //delete args.n
+        }
+        delete args.n
+        if (args.url == undefined) {
+            args.url = args.u
+            //delete args.u
+        }
+        delete args.u
+        if (args.help == undefined) {
+            args.help = args.h
+            //delete args.u
+        }
+        delete args.h
+        if (args.install == undefined) {
+            args.install = args.i
+            //delete args.u
+        }
+        delete args.i
+        if (args.remove == undefined) {
+            args.remove = args.r
+            //delete args.u
+        }
+        delete args.r
+        //console.log(args)
+        Object.keys(args).forEach(function (value, index) {
+            if (Object.values(args)[index] == undefined) {
+                delete args[value]
+            }
+        })
+        return args
+    },
+
+    downloadFavicon: async (url, name,cwd) => {
+        try {
+            
+            await download(`https://www.google.com/s2/favicons?domain_url=${url}&sz=128`, path.join(cwd, '/images/'))
+            fs.renameSync(path.join(cwd, '/images/', 'favicons.png'), path.join(cwd, '/images/', name + '.png'))
+
+
+            console.log('\x1b[32m', 'Icon Download Completed')
+
+        } catch (error) {
+            console.error('\x1b[31m', 'Error Downloading App Icon')
+            console.log(error)
+        }
+    },
+    createDesktopFile: (name, exec, image) => {
+        var file = `
+    [Desktop Entry]
+    Encoding=UTF-8
+    Version=1.0
+    Name=${name.replaceAll(' ', '_')}
+    GenericName=${name}
+    Exec=${exec}
+    Terminal=false
+    Icon=${image}
+    Type=Application
+    Categories=Application
+    Comment=Native Application for ${name}
+    `
+        fs.writeFileSync(`/usr/share/applications/${name}.desktop`, file)
+    },
+}
+var argsFunctions = {
+    install: async (args,cwd) => {
+        if (!(os.userInfo().uid === 0)) {
+            console.error('\x1b[31m', 'Error: This Program Must be Run as Root')
+            process.exit(1)
+        }
+        if (args.name == undefined) {
+            args.name = readline.question('What do you want to name this application:\n')
+        }
+        
+        var name = args.name
+        var url = args.install
+        if (readline.keyInYN(`Are you sure you want to install ${name}?`)) {
+            let database = JSON.parse(fs.readFileSync(path.join(cwd, 'apps.json')))
+            let image = path.join(cwd, '/images/', name + '.ico')
+            database[name] = { url, image }
+
+            fs.writeFileSync(path.join(cwd, 'apps.json'), JSON.stringify(database))
+            helperFunctions.createDesktopFile(name, `sh -c 'XAPP_FORCE_GTKWINDOW_ICON="${image}" && chromium-browser  --new-window --app=${url}'`, image)
+
+            await helperFunctions.downloadFavicon(url + '/favicon.ico', name,cwd).then(function () {
+                process.exit()
+            })
+            //process.exit()
+        } else { return }
+    },
+    run: (args,cwd) => {
+        //Run an app
+        var database = JSON.parse(fs.readFileSync(path.join(cwd, 'apps.json')))
+        exec(`chromium-browser  --new-window --app=${database[args.run].url}`)
+    },
+    help: () => {
+        if (process.argv[0] == 'sudo') {
+            var name = `sudo ${process.argv[1]}`
+        } else if (process.argv[0].includes('node')) {
+            var name = `node ${process.argv[1]}`
+        } else { var name = process.argv[0] }
+        console.log(`
+        Web Apps
+
+        Usage: ${name} <Action> <Options>
+        Actions:
+            install - Install a site as a native application(Must be run as sudo or root.)
+            run - Run a site that was installed as a native app 
+            remove - remove a site's application
+            help - Print this manual 
+        Options:
+            install - url*, name
+            run - name*
+            remove - name*
+        Examples:
+        ${name} run foo
+        ${name} install https://www.google.com --name google
+        ${name} install https://www.google.com 
+        ${name} remove google
+        ${name} help    
+        * states that that option is manditory     
+        
+        webapps@1.0.0 ${__dirname}      
+        `)
+        process.exit()
+    },
+    remove: (args,cwd) => {
+        function toUppercase(str) {
+            const arr = str.split(" ");
+
+            //loop through each element of the array and capitalize the first letter.
+
+
+            for (var i = 0; i < arr.length; i++) {
+                arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+            }
+            return arr.join('')
+        }
+        var database = JSON.parse(fs.readFileSync(path.join(cwd, 'apps.json')))
+        if (!(database[args.remove] == undefined)) {
+            if (readline.keyInYN(`Are you sure you want to remove ${args.remove}?`)) {
+                try {
+                    if (!(os.userInfo().uid === 0)) {
+                        console.error('\x1b[31m', 'Error: This Program Must be Run as Root')
+                        process.exit(1)
+                    } 
+                    
+                fs.unlinkSync(database[args.remove].image)
+                fs.unlinkSync(`/usr/share/applications/${args.remove}.desktop`)
+                delete database[args.remove]
+                fs.writeFileSync(path.join(cwd, 'apps.json'), JSON.stringify(database))
+                console.log('\x1b[32m',`${toUppercase(args.remove)} has been deleted`)
+                process.exit()
+                } catch (error) {
+                    console.log('\x1b[31m', `Error: Couldn't completly remove ${toUppercase(args.remove)}. This is proably due to a failed installation or a invalid parameter.`)
+                    //console.log(error)
+                }
+                
+            } else { return }
+        } else { console.log('\x1b[31m', `Error: ${toUppercase(args.remove)} doesn't exist.`) }
+    }
+};
+(async () => {
+    var args = helperFunctions.processArgs()
+    //console.log(args)
+    if (Object.values(args).every(el => el === undefined)) {
+        argsFunctions.help()
+        //console.log('test')
+    }
+    var cwd;
+    if (!(args.appDir == undefined)) {
+        cwd = args.appDir
+    }else{
+        cwd = '/system/webapps/'
+    }
+    Object.keys(args).forEach(async (key) => {
+        try {
+            if (key.includes('help')) {
+                await argsFunctions.help(args,cwd)
+
+            } else if (key.includes('install')) {
+                await argsFunctions.install(args,cwd)
+            } else if (key.includes('run')) {
+                await argsFunctions.run(args,cwd)
+            } else if (key.includes('remove')) {
+                await argsFunctions.remove(args,cwd)
+            }
+
+
+        } catch (err) {
+            console.log('\x1b[31m','Error: Invalid Argument')
+            console.error(err)
+            //argsFunctions.help()
+            process.exit(1)
+        }
+    })
+})()
