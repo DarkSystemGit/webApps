c87c4dbb16d636c62c862d8770179b9a0e174b45
darksystem <darksystemcoder@gmail.com>
2023-06-30 13:47:06 -0400

    installation can now be completed without node js

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo 'Starting Installation'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+nvm use node
+
 npm install
 echo 'Installing The Packging Module'
 npm install -g pkg
@@ -13,6 +19,8 @@ mv ./shell-linux ./builds/
 mv ./shell-win.exe ./builds/
 mv ./shell-macos ./builds/   
 npm remove -g pkg
+nvm uninstall node
+rm -rf "$NVM_DIR"
 echo 'Installation Complete. The executables are in the builds folder'
 if [ $(uname) == 'Linux' ]; then
     echo 'mv ./builds/shell-linux ./builds/webapps && mv ./builds/webapps /bin/'| sudo bash
